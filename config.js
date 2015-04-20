ec7e628b841adcf45c3ea2891d398a3d48d81d74
jpray <John.Pray@us.ing.com>
2015-04-20 01:34:41 -0400

    added tests

--- config.js
+++ config.js
@@ -13,16 +13,111 @@ System.config({
   }
 });
 
-// System.config({
-//   "meta": {
-//     "test": {
-//       "deps": [
-//         "lib/marionette-shim"
-//       ],
-//       //"format": "amd" // or try AMD
-//     }
-//   }
-// });
+System.config({
+  "meta": {
+    "b-amd-shims-amd": {
+      "deps": [
+        "a-amd-shims-amd"
+      ],
+      "format": "amd"
+    },
+    "b-amd-shims-cjs": {
+      "deps": [
+        "a-amd-shims-cjs"
+      ],
+      "format": "cjs"
+    },
+    "b-amd-shims-es6": {
+      "deps": [
+        "a-amd-shims-es6"
+      ],
+      "format": "es6"
+    },
+    "b-amd-shims-global": {
+      "deps": [
+        "a-amd-shims-global"
+      ],
+      "format": "global"
+    },
+    //
+    "b-cjs-shims-amd": {
+      "deps": [
+        "a-cjs-shims-amd"
+      ],
+      "format": "amd"
+    },
+    "b-cjs-shims-cjs": {
+      "deps": [
+        "a-cjs-shims-cjs"
+      ],
+      "format": "cjs"
+    },
+    "b-cjs-shims-es6": {
+      "deps": [
+        "a-cjs-shims-es6"
+      ],
+      "format": "es6"
+    },
+    "b-cjs-shims-global": {
+      "deps": [
+        "a-cjs-shims-global"
+      ],
+      "format": "global"
+    },
+    //
+    // THROWS MIXED DEPENDENCY ERROR
+    // "b-es6-shims-amd": {
+    //   "deps": [
+    //     "a-es6-shims-amd"
+    //   ],
+    //   "format": "amd"
+    // },
+    // THROWS MIXED DEPENDENCY ERROR
+    // "b-es6-shims-cjs": {
+    //   "deps": [
+    //     "a-es6-shims-cjs"
+    //   ],
+    //   "format": "cjs"
+    // },
+    "b-es6-shims-es6": {
+      "deps": [
+        "a-es6-shims-es6"
+      ],
+      "format": "es6"
+    },
+    "b-es6-shims-global": {
+      "deps": [
+        "a-es6-shims-global"
+      ],
+      "format": "global"
+    },
+    
+    "b-global-shims-amd": {
+      "deps": [
+        "a-global-shims-amd"
+      ],
+      "format": "amd"
+    },
+    "b-global-shims-cjs": {
+      "deps": [
+        "a-global-shims-cjs"
+      ],
+      "format": "cjs"
+    },
+    "b-global-shims-es6": {
+      "deps": [
+        "a-global-shims-es6"
+      ],
+      "format": "es6"
+    },
+    "b-global-shims-global": {
+      "deps": [
+        "a-global-shims-global"
+      ],
+      "format": "global"
+    },
+  }
+});
 
 System.config({
   "map": {
